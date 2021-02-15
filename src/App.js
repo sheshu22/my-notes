9e21cf07c72072fedf6576ec9e289762e00da073
Sheshasena Reddy <53706557+sheshu22@users.noreply.github.com>
2021-02-16 00:51:24 +0530

    Note Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Header from "./header";
 import Footer from "./footer";
+import Note from "./note";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Note />
       <Footer />
     </div>
   );
